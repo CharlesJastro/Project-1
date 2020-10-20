c67b43d13b361c237361048919f8537598d68998
CharlesJastro <71402570+CharlesJastro@users.noreply.github.com>
2020-10-20 12:05:32 -0600

    Add files via upload

--- MyProject1.js
+++ MyProject1.js
@@ -13,6 +13,21 @@ for (i = 0; i < coll.length; i++) {
   });
 }
 
+var coll = document.getElementsByClassName("projects");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("minusSign");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
+
 var coll = document.getElementsByClassName("interests");
 var i;
 
